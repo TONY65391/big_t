bde18a641d69bb1422f04d8d868f3377fb401772
TONY65391 <tonyanthony65391@gmail.com>
2024-12-13 09:57:34 -0800

    views commit

--- FB/urls.py
+++ FB/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('create/', views.create),
-    path('create/database/', views.database, name = 'database'),
+    #path('create/database/', views.database, name = 'database'),
     #path('create/database/<int:id>', views.database, name = 'database')
 ]
--- FB/views.py
+++ FB/views.py
@@ -21,21 +21,3 @@ def create(request):
     context = {'forms':form}
     template = loader.get_template('create.html')
     return HttpResponse(template.render(request = request, context = context))
-
-gmail = []
-id = []
-firstname = []
-lastname = []
-
-
-def database(request):
-    if request.GET["id"] and request.GET["First_Name"] and request.GET["Last_Name"] and request.GET["Gmail"]:
-        if request.GET["Gmail"] in gmail:
-            return HttpResponse(f"{request.GET["Gmail"]} already exists")
-        
-        gmail.append(request.GET["Gmail"])
-        id.append(request.GET["id"])
-        firstname.append(request.GET["First_Name"])
-        lastname.append(request.GET["Last_Name"])
-        print(id,firstname,lastname,gmail)
-        return HttpResponse(f"{request.GET["First_Name"]} {request.GET["Last_Name"]} created")
